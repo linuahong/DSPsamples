e6ff562dba564c667ea90ad0b428930683565869
Administrator <Administrator@USER-20150403NC>
2016-08-12 10:10:00 +0800

    modified!!!

--- button/Debug/main.pp
+++ button/Debug/main.pp
@@ -1,9 +1,9 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: D:/ccs5.2/ccsv5/tools/compiler/c6000_7.3.4/include/stdio.h
-main.obj: D:/ccs5.2/ccsv5/tools/compiler/c6000_7.3.4/include/linkage.h
-main.obj: D:/ccs5.2/ccsv5/tools/compiler/c6000_7.3.4/include/stdarg.h
+main.obj: D:/CCS5.5.0/ccsv5/tools/compiler/c6000_7.4.4/include/stdio.h
+main.obj: D:/CCS5.5.0/ccsv5/tools/compiler/c6000_7.4.4/include/linkage.h
+main.obj: D:/CCS5.5.0/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h
 main.obj: ../include/hw/hw_types.h
 main.obj: ../include/hw/hw_syscfg0_OMAPL138.h
 main.obj: ../include/hw/soc_OMAPL138.h
@@ -12,9 +12,9 @@ main.obj: ../include/hw/hw_gpio.h
 main.obj: ../include/c674x/omapl138/interrupt.h
 
 ../main.c: 
-D:/ccs5.2/ccsv5/tools/compiler/c6000_7.3.4/include/stdio.h: 
-D:/ccs5.2/ccsv5/tools/compiler/c6000_7.3.4/include/linkage.h: 
-D:/ccs5.2/ccsv5/tools/compiler/c6000_7.3.4/include/stdarg.h: 
+D:/CCS5.5.0/ccsv5/tools/compiler/c6000_7.4.4/include/stdio.h: 
+D:/CCS5.5.0/ccsv5/tools/compiler/c6000_7.4.4/include/linkage.h: 
+D:/CCS5.5.0/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h: 
 ../include/hw/hw_types.h: 
 ../include/hw/hw_syscfg0_OMAPL138.h: 
 ../include/hw/soc_OMAPL138.h: 
